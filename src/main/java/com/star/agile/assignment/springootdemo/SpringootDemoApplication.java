ebf665700208f40ccd3b625dc78dbe2f8860ee3a
rajeswari69 <rajeswarigottipalli69@gmail.com>
2023-04-29 12:30:04 +0530

    adding springoot application

---
+++ src/main/java/com/star/agile/assignment/springootdemo/SpringootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/star/agile/assignment/springootdemo/SpringootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
